69f0d51cbf2b4612951e30b3dfe145760d3a4036
Nikolai E <starnuik@gmail.com>
2025-10-27 14:43:48 +0300

    login reward

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+from json import JSONEncoder
+import json
 from pathlib import Path
+import random
 import grpc
 import threading
 import logging
@@ -8,7 +11,7 @@ from concurrent import futures
 from dependency_injector import containers, providers
 from dependency_injector.wiring import Provide, inject
 from logging import Logger
-from sqlalchemy import Text, BigInteger, Integer, ForeignKey, Identity, Column, Engine, create_engine, select
+from sqlalchemy import Text, BigInteger, Integer, ForeignKey, Identity, Column, Engine, create_engine, select, update
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 from jwt.exceptions import PyJWTError
 
@@ -36,30 +39,50 @@ class Item(ModelBase):
 
 JWT_SECRET = "meow"
 JWT_ALGO = "HS256"
+JWT_PAYLOAD_KEY = "__payload"
+JWT_HEADER = "session_token"
 
-def jwt_pack(username: str) -> str:
-	payload = {
-		"username": username,
+class SessionPayload():
+	def __init__(self, username):
+		self.username = username
+	def as_json(self):
+		return json.dumps(self, default = lambda obj : obj.__dict__)
+
+def as_payload(jsn: str) -> SessionPayload:
+	dct = json.loads(jsn)
+	return SessionPayload(dct["username"])
+
+def jwt_pack(payload: SessionPayload) -> str:
+	jwt_payload = {
+		JWT_PAYLOAD_KEY: payload.as_json(),
 	}
-	token = jwt.encode(payload, JWT_SECRET, algorithm = JWT_ALGO)
+	token = jwt.encode(jwt_payload, JWT_SECRET, algorithm = JWT_ALGO)
 	return token
 
-def jwt_unpack(token: str) -> str | None:
+def jwt_unpack(token: str) -> SessionPayload | None:
 	try:
-		payload = jwt.decode(token, JWT_SECRET, algorithms = [JWT_ALGO])
-		return payload["username"]
+		jwt_payload = jwt.decode(token, JWT_SECRET, algorithms = [JWT_ALGO])
+		payload_string = jwt_payload[JWT_PAYLOAD_KEY]
+		payload = as_payload(payload_string)
+		return payload
 	except PyJWTError:
 		return None
 
-def username_from_metadata(metadata: tuple[tuple[str, str | bytes], ...]) -> str | None:
-	metadata = dict(metadata)
-
-	if not "session_token" in metadata:
+def jwt_from_metadata(metadata: dict[str, str | bytes]) -> SessionPayload | None:
+	if not JWT_HEADER in metadata:
 		return None
-	token = metadata["session_token"]
+	token = metadata[JWT_HEADER]
 
-	username = jwt_unpack(token)
-	return username
+	payload = jwt_unpack(token)
+	return payload
+
+def jwt_from_details(details: grpc.HandlerCallDetails) -> SessionPayload | None:
+	metadata = dict(details.invocation_metadata)
+	return jwt_from_metadata(metadata)
+
+def jwt_from_context(context: grpc.ServicerContext) -> SessionPayload | None:
+	metadata = dict(context.invocation_metadata())
+	return jwt_from_metadata(metadata)
 
 class AuthInterceptor(grpc.ServerInterceptor):
 	def __init__(self):
@@ -73,10 +96,8 @@ class AuthInterceptor(grpc.ServerInterceptor):
 		if path.parts[1] == "Auth":
 			return continuation(handler_call_details)
 		
-		print(dict(handler_call_details.invocation_metadata))
-
-		username = username_from_metadata(handler_call_details.invocation_metadata)
-		if username == None:
+		payload = jwt_from_details(handler_call_details)
+		if payload == None:
 			return self.abort_handler
 		
 		resp = continuation(handler_call_details)
@@ -88,7 +109,6 @@ class LoggingInterceptor(grpc.ServerInterceptor):
 		self.log = logger
 	
 	def intercept_service(self, continuation, handler_call_details):
-		handler_call_details.invocation_metadata
 		resp = continuation(handler_call_details)
 
 		msg = "success" if resp != None else "fail"
@@ -97,32 +117,65 @@ class LoggingInterceptor(grpc.ServerInterceptor):
 		return resp
 
 class Model:
-	def __init__(self, session_factory: providers.Callable[Session]):
+	def __init__(self,
+		session_factory: providers.Callable[Session]
+	):
 		self.session = session_factory
-	
+
+	# Read
 	def user_exists(self, username: str) -> bool:
 		with self.session() as session:
 			stmt = select(User).where(User.username == username)
 			return session.scalars(stmt).first() != None
 	
+	def get_user(self, username: str) -> User:
+		with self.session() as session:
+			stmt = select(User).where(User.username == username)
+			return session.scalars(stmt).one()
+
+	# Update
 	def create_user(self, username: str):
 		with self.session() as session:
 			session.add(User(username = username))
 			session.commit()
+		
+	def add_credits(self, username: str, amount: int):
+		with self.session() as session:
+			stmt = update(User) \
+				.where(User.username == username) \
+				.values(balance = User.balance + amount)
+			session.execute(stmt)
+			session.commit()
 
 class Servicer(api.AuthServicer, api.MetaServicer):
-	def __init__(self, logger: Logger, model: Model):
+	def __init__(self,
+		logger: Logger,
+		model: Model,
+		reward_amount_factory: providers.Callable[int],
+	):
 		self.log = logger
 		self.model = model
+		self.reward_amount = reward_amount_factory
 
-	def Login(self, req: dto.LoginReq, context: grpc.ServicerContext):
+	def Login(self, req: dto.LoginReq, context):
 		if not self.model.user_exists(req.username):
 			self.model.create_user(req.username)
-		token = jwt_pack(req.username)
+		payload = SessionPayload(req.username)
+		token = jwt_pack(payload)
 		return dto.UserSession(session_token = token)
 
-	def Login2(self, req: dto.Empty, context):
-		return dto.UserData(username = "TODO", credits = -1, items = [],)
+	def Login2(self, _, context):
+		payload = jwt_from_context(context)
+		username = payload.username
+		reward = self.reward_amount()
+		self.model.add_credits(username, reward)
+		return self.GetUserData(_, context)
+
+	def GetUserData(self, _, context):
+		payload = jwt_from_context(context)
+		username = payload.username
+		user = self.model.get_user(username)
+		return dto.UserData(username = username, credits = user.balance, items = [],)
 
 def connect_database() -> Engine:
 	engine = create_engine("sqlite:///db.sqlite3")
@@ -157,10 +210,16 @@ class Container(containers.DeclarativeContainer):
 	)
 
 	# business
+	reward_amount_factory = providers.Factory(
+		random.randrange,
+		start = config.data.credits_reward.min,
+		stop = config.data.credits_reward.max,
+	)
 	servicer = providers.Singleton(
 		Servicer,
 		logger = logger,
 		model = model,
+		reward_amount_factory = reward_amount_factory.provider,
 	)
 
 def run(container: Container):
